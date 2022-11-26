750ee17a37512ca450e79f6fc562dee5f5be1443
ltiseanu <ltiseanu@bitdefender.com>
2022-11-26 21:11:57 +0200

    Addin a model class creating an object form doto list array of obj

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import ToDos from './components/ToDos';
 import ToDoInterface from './models/todo';
 
 function App() {
-  const todoArr:string[] = ['Learn React', 'Learn Typescript']; // this is an array of strings
-  
+  // const todoArr:string[] = ['Learn React', 'Learn Typescript']; // this is an array of strings
+  const todos = [ // this is now an array of objects
+    new ToDoInterface('Learn React'),
+    new ToDoInterface('Learn Typescript')
+  ];
 
   return (
     <div>
-      <ToDos items={todoArr} />
+      <ToDos items={todos} />
     </div>
   );
 }
--- src/components/ToDos.tsx
+++ src/components/ToDos.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
+import ToDoInterface from "../models/todo";
 import ToDoItem from "./ToDoItem";
 
 type TypeValuesProps = {
-    items: string[]
+    items: ToDoInterface[]
 };
 
 const ToDos: React.FC<TypeValuesProps> = (props) => {
     return (
         <ul>
             {props.items.map((item, k) => (
-                <ToDoItem key={k} name={item} />
+                <ToDoItem key={k} name={item.text} />
             ))}
             {/* {props.items.map((item, k) => <li key={k}>{item}</li>)} */}
 
